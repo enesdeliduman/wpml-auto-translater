3ccc431798291e1ce0ceefcc09998f874aa2fd76
Enes Deliduman <eduman931@gmail.com>
2024-10-23 12:46:08 +0300

    Page fields translation completed

--- Helpers/DeeplHelper.js
+++ Helpers/DeeplHelper.js
@@ -1,8 +1,15 @@
 const deepl = require("deepl-node")
 
-module.exports.DeeplTranslator = async (message) => {
-    const translator = new deepl.Translator(process.env.DEEPL_API_KEY)
-    const result = await translator.translateText(message, null, 'en-US');
-    var text=result.text
-    return text
+module.exports.DeeplTranslator = async (message, lang) => {
+    try {
+        lang == "en" ? lang = "en-US" : lang = lang;
+        console.log(lang);
+        const translator = new deepl.Translator(process.env.DEEPL_API_KEY)
+        const result = await translator.translateText(message, null, lang);
+        var text = result.text
+        return text
+    }
+    catch (err) {
+        console.error(err)
+    }
 }
--- server.js
+++ server.js
@@ -6,48 +6,91 @@ const deeplHelper = require("./Helpers/DeeplHelper");
     let driver = await new Builder().forBrowser('chrome').build();
 
     try {
-        await driver.get(process.env.ADMIN_LOGIN_PANEL);  // Giriş URL'nizi değiştirin
+        await driver.get(process.env.ADMIN_LOGIN_PANEL);
         await driver.findElement(By.id('user_login')).sendKeys(process.env.ADMIN_USERNAME);
         await driver.findElement(By.id('user_pass')).sendKeys(process.env.ADMIN_PASS);
         await driver.findElement(By.id('wp-submit')).click();
         await driver.get(process.env.PAGES);
-        let wpmlLinks = await driver.findElements(By.className('js-wpml-translate-link'));
-        for (let wpmlLink of wpmlLinks) {
-            try {
-                await wpmlLink.click();
-                await driver.wait(until.elementLocated(By.css('.wpml-form-row:not(.hidden)')), 5000);
-                let visibleDivs = await driver.findElements(By.css('.wpml-form-row:not(.hidden)'));
-                for (let div of visibleDivs) {
-                    try {
-                        let originalValueInput = await div.findElement(By.className('original_value'));
-                        let translatedValueInput = await div.findElement(By.className('translated_value'));
-                        let originalValue = await originalValueInput.getAttribute('value');
-                        let translatedValue = await translatedValueInput.getAttribute('value');
-                        await translatedValueInput.clear();
-                        let newValue = await deeplHelper.DeeplTranslator(originalValue);
-                        await translatedValueInput.sendKeys(newValue);
 
-                        console.log(`Orijinal: ${originalValue} | Çeviri: ${newValue}`);
-                    } catch (error) {
-                        if (error.name === 'StaleElementReferenceError') {
-                            console.log('Geçersiz öğe referansı, öğeyi yeniden bulmayı deniyorum...');
-                            let updatedDiv = await driver.findElement(By.css(`.wpml-form-row:not(.hidden)`));
-                            let originalValueInput = await updatedDiv.findElement(By.className('original_value'));
-                            let translatedValueInput = await updatedDiv.findElement(By.className('translated_value'));
-                        } else {
-                            console.error('Bir hata oluştu:', error);
+        let hasMorePages = true;
+
+        while (hasMorePages) {
+            await driver.wait(until.elementLocated(By.className('js-wpml-translate-link')), 5000);
+            let wpmlLinks = await driver.findElements(By.className('js-wpml-translate-link'));
+            let filteredLinks = [];
+
+            for (let wpmlLink of wpmlLinks) {
+                let icon = await wpmlLink.findElements(By.css('i.otgs-ico-add.js-otgs-popover-tooltip'));
+                let originalLink = await wpmlLink.getAttribute('data-original-link');
+
+                if (icon.length > 0 && originalLink && originalLink.startsWith('post-new.php')) {
+                    filteredLinks.push(wpmlLink);
+                }
+            }
+
+            if (filteredLinks.length === 0) {
+                console.log('Tercüme edilecek daha fazla bağlantı yok.');
+                hasMorePages = false; 
+                break;
+            }
+
+            for (let wpmlLink of filteredLinks) {
+                try {
+                    let originalLink = await wpmlLink.getAttribute('data-original-link');
+                    let urlParams = new URLSearchParams(originalLink.split('?')[1]);
+                    let languageCode = urlParams.get('lang') || 'en-US';
+                    await wpmlLink.click();
+                    await driver.wait(until.elementLocated(By.css('.wpml-form-row:not(.hidden)')), 5000);
+
+                    let visibleDivs = await driver.findElements(By.css('.wpml-form-row:not(.hidden)'));
+                    for (let div of visibleDivs) {
+                        try {
+                            let originalValueInput = await div.findElement(By.className('original_value'));
+                            let originalValue = await originalValueInput.getAttribute('value');
+
+                            let newValue = await deeplHelper.DeeplTranslator(originalValue, languageCode);
+                            let translatedValueInput = await div.findElement(By.className('translated_value'));
+
+                            await translatedValueInput.clear();
+                            await translatedValueInput.sendKeys(newValue);
+
+                            console.log(`Orijinal: ${originalValue} | Çeviri: ${newValue} | Dil Kodu: ${languageCode}`);
+
+                            try {
+                                let checkbox = await div.findElement(By.css('.icl_tm_finished.js-field-translation-complete'));
+                                await checkbox.click();
+                            } catch (error) {
+                                console.error('Checkbox işaretleme hatası:', error);
+                            }
+
+                        } catch (error) {
+                            if (error.name === 'StaleElementReferenceError') {
+                                console.log('Geçersiz öğe referansı, öğeyi yeniden bulmayı deniyorum...');
+                                div = await driver.findElement(By.css('.wpml-form-row:not(.hidden)'));
+                                let originalValueInput = await div.findElement(By.className('original_value'));
+                                let originalValue = await originalValueInput.getAttribute('value');
+
+                                let newValue = await deeplHelper.DeeplTranslator(originalValue, languageCode);
+                                let translatedValueInput = await div.findElement(By.className('translated_value'));
+
+                                await translatedValueInput.clear();
+                                await translatedValueInput.sendKeys(newValue);
+                            } else {
+                                console.error('Bir hata oluştu:', error);
+                            }
                         }
                     }
-                }
-                // await driver.findElement(By.className("button button-primary button-large wpml-dialog-close-button js-save-and-close")).click()
 
-                // await driver.navigate().back();
-            } catch (error) {
-                console.error('Linke tıklama sırasında bir hata oluştu:', error);
+                    let saveButton = await driver.wait(until.elementLocated(By.className("button button-primary button-large wpml-dialog-close-button js-save-and-close")), 5000);
+                    await saveButton.click();
+                    await driver.wait(until.urlIs(process.env.PAGES), 10000);
+                } catch (error) {
+                    console.error('Linke tıklama sırasında bir hata oluştu:', error);
+                }
             }
         }
 
     } finally {
-        // await driver.quit();
+        await driver.quit();
     }
 })();
